7a94f6030f08aaf9c6d0162ac90d5c7b316c5feb
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-18 18:08:20 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 function App() {
   return (
-    <div>
-      <h1 className="text-danger">Blood Bank App</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
