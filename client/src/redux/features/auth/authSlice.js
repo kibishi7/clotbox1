92c4c7e4c21de9aed865165427445be7d09e742a
Pratham Raj Bhatt <prathamrajbhatt456@gmail.com>
2023-06-25 03:28:43 +0530

    register action and reducer

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,46 +1,63 @@
-import { userLogin, userRegister } from "../redux/features/auth/authActions";
-import store from "../redux/store";
+import { createSlice } from "@reduxjs/toolkit";
+import { getCurrentUser, userLogin, userRegister } from "./authActions";
 
-export const handleLogin = (e, email, password, role) => {
-  e.preventDefault();
-  try {
-    if (!role || !email || !password) {
-      return alert("Please Privde All Feilds");
-    }
-    store.dispatch(userLogin({ email, password, role }));
-  } catch (error) {
-    console.log(error);
-  }
+const token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+
+const initialState = {
+  loading: false,
+  user: null,
+  token,
+  error: null,
 };
 
-export const handleRegister = (
-  e,
-  name,
-  role,
-  email,
-  password,
-  phone,
-  organisationName,
-  address,
-  hospitalName,
-  website
-) => {
-  e.preventDefault();
-  try {
-    store.dispatch(
-      userRegister({
-        name,
-        role,
-        email,
-        password,
-        phone,
-        organisationName,
-        address,
-        hospitalName,
-        website,
-      })
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // login user
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+      state.token = payload.token;
+    });
+    builder.addCase(userLogin.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // REGISTER user
+    builder.addCase(userRegister.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(userRegister.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // CURRENT user
+    builder.addCase(getCurrentUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+  },
+});
+
+export default authSlice;
