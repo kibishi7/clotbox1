2fea1581a5e5cd8dd15f2c16c684f4877223bfe4
Priyanshu Goyal <goyalpriyanshu814@gmail.com>
2023-06-26 03:32:11 +0530

    out action added

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const inventoryModel = require("../models/inventoryModel");
 const userModel = require("../models/userModel");
 
@@ -10,12 +11,57 @@ const createInventoryController = async (req, res) => {
     if (!user) {
       throw new Error("User Not Found");
     }
-    if (inventoryType === "in" && user.role !== "donar") {
-      throw new Error("Not a donar account");
-    }
-    if (inventoryType === "out" && user.role !== "hospital") {
-      throw new Error("Not a hospital");
-    }
+
+    // if (inventoryType === "in" && user.role !== "donar") {
+    //   throw new Error("Not a donar account");
+    // }
+    // if (inventoryType === "out" && user.role !== "hospital") {
+    //   throw new Error("Not a hospital");
+    // }
+
+    //if out is choosen-->
+    if (req.body.inventoryType == "out") {
+      const requestedBloodGroup = req.body.bloodGroup;
+      const requestedQuantityOfBlood = req.body.quantity;
+      const organisation = new mongoose.Types.ObjectId(req.body.userId);
+
+            //calculate Blood Quanitity
+            const totalInOfRequestedBlood = await inventoryModel.aggregate([
+              {
+                $match: {
+                  organisation,
+                  inventoryType: "in",
+                  bloodGroup: requestedBloodGroup,
+                },
+              },
+              {
+                  $group: {
+                    _id: "$bloodGroup",
+                    total: { $sum: "$quantity" },
+                  },
+                },
+              ]);
+
+const totalIn = totalInOfRequestedBlood[0]?.total || 0;
+               //calculate 'OUT' Blood Quanitity
+ 
+      const totalOutOfRequestedBloodGroup = await inventoryModel.aggregate([
+        {
+          $match: {
+            organisation,
+            inventoryType: "out",
+            bloodGroup: requestedBloodGroup,
+          },
+        },
+        {
+          $group: {
+            _id: "$bloodGroup",
+            total: { $sum: "$quantity" },
+          },
+        },
+      ]);
+      const totalOut = totalOutOfRequestedBloodGroup[0]?.total || 0;
+
     //save record
     const inventory = new inventoryModel(req.body);
     await inventory.save();
