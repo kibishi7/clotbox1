36bf517a3e94c5bbd23c78dcb06f5ccdb5d392aa
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-25 15:34:11 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 //enabling routing function
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './redux/store';
+import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-    
   </React.StrictMode>
 );
 
