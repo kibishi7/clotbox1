88c69d155be34d4adb43f06c896ca8eb61d80618
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-26 16:52:41 +0530

    Update inventoryRoutes.js

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -19,8 +19,6 @@ router.post("/create-inventory", authMiddelware, createInventoryController);
 
 //GET ALL BLOOD RECORDS
 router.get("/get-inventory", authMiddelware, getInventoryController);
-
-
 //GET RECENT BLOOD RECORDS
 router.get(
   "/get-recent-inventory",
