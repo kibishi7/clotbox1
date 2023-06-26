1ce2558a30d09f99285e39bc82835563a4fd2db0
ankpal145 <97392986+ankpal145@users.noreply.github.com>
2023-06-26 16:52:18 +0530

    Update Organisationpage.js

--- client/src/pages/Dashboard/Organisationpage.js
+++ client/src/pages/Dashboard/Organisationpage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Layout from "./../../components/shared/Layout/Layout";
+import Layout from "../../components/shared/Layout/Layout";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import API from "../../services/API";
